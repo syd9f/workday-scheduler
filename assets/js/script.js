c52f433d5adcf353fffaf496070d9f753de45244
sydnief <sydniefarrell@gmail.com>
2023-03-03 13:25:48 -0500

    fixed code so color displays correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,116 +10,200 @@ $(function () {
   // useful when saving the description in local storage?
   // 
   
-  // select save button for each element
-  var saveButton = $('#hour-9').children('.saveButton');
+  // // select save button for each element
+  // var saveButton = $('#hour-9').children('.saveButton');
 
-  saveButton.addEventListener('click', saveButton);
-  // select user input
-  var contentNine = $('.nineAm-text').textValue;
-  var contentTen = $('.tenAm-text').textValue;
-  var contentEleven = $('.elevenAm-text').textValue;
-  var contentTwelve = $('.twelvePm-text').textValue;
-  var contentOne = $('.onePm-text').textValue;
-  var contentTwo = $('.twoPm-text').textValue;
-  var contentThree = $('.threePm-text').textValue;
-  var contentFour = $('.fourPm-text').textValue;
-  var contentFive = $('.fivePm-text').textValue;
+  // saveButton.addEventListener('click', saveButton);
+  // // select user input
+  // var contentNine = $('.nineAm-text').textValue;
+  // var contentTen = $('.tenAm-text').textValue;
+  // var contentEleven = $('.elevenAm-text').textValue;
+  // var contentTwelve = $('.twelvePm-text').textValue;
+  // var contentOne = $('.onePm-text').textValue;
+  // var contentTwo = $('.twoPm-text').textValue;
+  // var contentThree = $('.threePm-text').textValue;
+  // var contentFour = $('.fourPm-text').textValue;
+  // var contentFive = $('.fivePm-text').textValue;
 
-  // save to local storage
-  localStorage.setItem("nineAm", contentNine);
-  localStorage.setItem("tenAm", contentTen);
-  localStorage.setItem("elevenAm", contentEleven);
-  localStorage.setItem("twelvePm", contentTwelve);
-  localStorage.setItem("onePm", contentOne);
-  localStorage.setItem("twoPm", contentTwo);
-  localStorage.setItem("threePm", contentThree);
-  localStorage.setItem("fourPm", contentFour);
-  localStorage.setItem("fivePm", contentFive);
+  // // save to local storage
+  // localStorage.setItem("nineAm", contentNine);
+  // localStorage.setItem("tenAm", contentTen);
+  // localStorage.setItem("elevenAm", contentEleven);
+  // localStorage.setItem("twelvePm", contentTwelve);
+  // localStorage.setItem("onePm", contentOne);
+  // localStorage.setItem("twoPm", contentTwo);
+  // localStorage.setItem("threePm", contentThree);
+  // localStorage.setItem("fourPm", contentFour);
+  // localStorage.setItem("fivePm", contentFive);
 
   // Set color of text boxes
   // 9 AM
   var nineAm = $('#hour-9');
-  var selectNine = dayjs().hour(9);
+  var selectNine = dayjs().hour(9).$H;
   if (dayjs().hour() == selectNine){
     nineAm.removeClass('future');
     nineAm.addClass('present');
   }
   // 10 AM
   var tenAm = $('#hour-10');
-  var selectTen = dayjs().hour(10);
+  var selectTen = dayjs().hour(10).$H;
   if (dayjs().hour() == selectTen){
     tenAm.removeClass('future');
     tenAm.addClass('present');
     // set 9AM to past
-    nineAm.removeClass('present');
+    nineAm.removeClass('present future');
     nineAm.addClass('past');
   }
   // 11 AM
   var elevenAm = $('#hour-11');
-  var selectEleven = dayjs().hour(11);
+  var selectEleven = dayjs().hour(11).$H;
   if (dayjs().hour() == selectEleven){
     elevenAm.removeClass('future');
     elevenAm.addClass('present');
+    // set 9AM to past
+    nineAm.removeClass('present future');
+    nineAm.addClass('past');
     // set 10AM to past
-    tenAm.removeClass('present');
+    tenAm.removeClass('present future');
     tenAm.addClass('past');
   }
   // 12 PM
   var twelvePm = $('#hour-12');
-  var selectTwelve = dayjs().hour(12);
+  var selectTwelve = dayjs().hour(12).$H;
   if (dayjs().hour() == selectTwelve){
     twelvePm.removeClass('future');
     twelvePm.addClass('present');
+    // set 9AM to past
+    nineAm.removeClass('present future');
+    nineAm.addClass('past');
+    // set 10AM to past
+    tenAm.removeClass('present future');
+    tenAm.addClass('past');
     // set 11AM to past
-    elevenAm.removeClass('present');
+    elevenAm.removeClass('present future');
     elevenAm.addClass('past');
   }
   // 1 PM
   var onePm = $('#hour-1');
-  var selectOne = dayjs().hour(13);
+  var selectOne = dayjs().hour(13).$H;
   if (dayjs().hour() == selectOne){
     onePm.removeClass('future');
     onePm.addClass('present');
+    // set 9AM to past
+    nineAm.removeClass('present future');
+    nineAm.addClass('past');
+    // set 10AM to past
+    tenAm.removeClass('present future');
+    tenAm.addClass('past');
+    // set 11AM to past
+    elevenAm.removeClass('present future');
+    elevenAm.addClass('past');
     // set 12PM to past
-    twelvePm.removeClass('present');
+    twelvePm.removeClass('present future');
     twelvePm.addClass('past');
   }
   // 2 PM
   var twoPm = $('#hour-2');
-  var selectTwo = dayjs().hour(14);
+  var selectTwo = dayjs().hour(14).$H;
   if (dayjs().hour() == selectTwo){
     twoPm.removeClass('future');
     twoPm.addClass('present');
+    // set 9AM to past
+    nineAm.removeClass('present future');
+    nineAm.addClass('past');
+    // set 10AM to past
+    tenAm.removeClass('present future');
+    tenAm.addClass('past');
+    // set 11AM to past
+    elevenAm.removeClass('present future');
+    elevenAm.addClass('past');
+    // set 12PM to past
+    twelvePm.removeClass('present future');
+    twelvePm.addClass('past');
     // set 1PM to past
     onePm.removeClass('present');
     onePm.addClass('past');
   }
   // 3 PM
   var threePm = $('#hour-3');
-  var selectThree = dayjs().hour(15);
+  var selectThree = dayjs().hour(15).$H;
   if (dayjs().hour() == selectThree){
     threePm.removeClass('future');
     threePm.addClass('present');
+    // set 9AM to past
+    nineAm.removeClass('present future');
+    nineAm.addClass('past');
+    // set 10AM to past
+    tenAm.removeClass('present future');
+    tenAm.addClass('past');
+    // set 11AM to past
+    elevenAm.removeClass('present future');
+    elevenAm.addClass('past');
+    // set 12PM to past
+    twelvePm.removeClass('present future');
+    twelvePm.addClass('past');
+    // set 1PM to past
+    onePm.removeClass('present');
+    onePm.addClass('past');
     // set 2PM to past
     twoPm.removeClass('present');
     twoPm.addClass('past');
   }
   // 4 PM
   var fourPm = $('#hour-4');
-  var selectFour = dayjs().hour(16);
+  var selectFour = dayjs().hour(16).$H;
   if (dayjs().hour() == selectFour){
     fourPm.removeClass('future');
     fourPm.addClass('present');
+    // set 9AM to past
+    nineAm.removeClass('present future');
+    nineAm.addClass('past');
+    // set 10AM to past
+    tenAm.removeClass('present future');
+    tenAm.addClass('past');
+    // set 11AM to past
+    elevenAm.removeClass('present future');
+    elevenAm.addClass('past');
+    // set 12PM to past
+    twelvePm.removeClass('present future');
+    twelvePm.addClass('past');
+    // set 1PM to past
+    onePm.removeClass('present');
+    onePm.addClass('past');
+    // set 2PM to past
+    twoPm.removeClass('present');
+    twoPm.addClass('past');
     // set 3PM to past
     threePm.removeClass('present');
     threePm.addClass('past');
   }
   // 5 PM
   var fivePm = $('#hour-5');
-  var selectFive = dayjs().hour(17);
+  var selectFive = dayjs().hour(17).$H;
   if (dayjs().hour() == selectFive){
     fivePm.removeClass('future');
     fivePm.addClass('present');
+    // set 9AM to past
+    nineAm.removeClass('present future');
+    nineAm.addClass('past');
+    // set 10AM to past
+    tenAm.removeClass('present future');
+    tenAm.addClass('past');
+    // set 11AM to past
+    elevenAm.removeClass('present future');
+    elevenAm.addClass('past');
+    // set 12PM to past
+    twelvePm.removeClass('present future');
+    twelvePm.addClass('past');
+    // set 1PM to past
+    onePm.removeClass('present');
+    onePm.addClass('past');
+    // set 2PM to past
+    twoPm.removeClass('present');
+    twoPm.addClass('past');
+    // set 3PM to past
+    threePm.removeClass('present');
+    threePm.addClass('past');
     // set 4PM to past
     fourPm.removeClass('present');
     fourPm.addClass('past');
