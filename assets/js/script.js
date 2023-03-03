d619c925a6227db0ed03043940c21ec048228fe0
sydnief <sydniefarrell@gmail.com>
2023-03-03 02:35:04 -0500

    created if statement to set time block color based on time

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,30 +8,95 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+  // 
+  
   // 9 AM
   var nineAm = $('#hour-9');
+  var selectNine = dayjs().hour(9);
+  if (dayjs().hour() == selectNine){
+    nineAm.removeClass('future');
+    nineAm.addClass('present');
+  }
   // 10 AM
   var tenAm = $('#hour-10');
+  var selectTen = dayjs().hour(10);
+  if (dayjs().hour() == selectTen){
+    tenAm.removeClass('future');
+    tenAm.addClass('present');
+    // set 9AM to past
+    nineAm.removeClass('present');
+    nineAm.addClass('past');
+  }
   // 11 AM
   var elevenAm = $('#hour-11');
+  var selectEleven = dayjs().hour(11);
+  if (dayjs().hour() == selectEleven){
+    elevenAm.removeClass('future');
+    elevenAm.addClass('present');
+    // set 10AM to past
+    tenAm.removeClass('present');
+    tenAm.addClass('past');
+  }
   // 12 PM
   var twelvePm = $('#hour-12');
+  var selectTwelve = dayjs().hour(12);
+  if (dayjs().hour() == selectTwelve){
+    twelvePm.removeClass('future');
+    twelvePm.addClass('present');
+    // set 11AM to past
+    elevenAm.removeClass('present');
+    elevenAm.addClass('past');
+  }
   // 1 PM
   var onePm = $('#hour-1');
+  var selectOne = dayjs().hour(13);
+  if (dayjs().hour() == selectOne){
+    onePm.removeClass('future');
+    onePm.addClass('present');
+    // set 12PM to past
+    twelvePm.removeClass('present');
+    twelvePm.addClass('past');
+  }
   // 2 PM
   var twoPm = $('#hour-2');
+  var selectTwo = dayjs().hour(14);
+  if (dayjs().hour() == selectTwo){
+    twoPm.removeClass('future');
+    twoPm.addClass('present');
+    // set 1PM to past
+    onePm.removeClass('present');
+    onePm.addClass('past');
+  }
   // 3 PM
   var threePm = $('#hour-3');
+  var selectThree = dayjs().hour(15);
+  if (dayjs().hour() == selectThree){
+    threePm.removeClass('future');
+    threePm.addClass('present');
+    // set 2PM to past
+    twoPm.removeClass('present');
+    twoPm.addClass('past');
+  }
   // 4 PM
   var fourPm = $('#hour-4');
+  var selectFour = dayjs().hour(16);
+  if (dayjs().hour() == selectFour){
+    fourPm.removeClass('future');
+    fourPm.addClass('present');
+    // set 3PM to past
+    threePm.removeClass('present');
+    threePm.addClass('past');
+  }
   // 5 PM
   var fivePm = $('#hour-5');
+  var selectFive = dayjs().hour(17);
+  if (dayjs().hour() == selectFive){
+    fivePm.removeClass('future');
+    fivePm.addClass('present');
+    // set 4PM to past
+    fourPm.removeClass('present');
+    fourPm.addClass('past');
+  }
 
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
