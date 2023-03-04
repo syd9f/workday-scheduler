8dce5e749141b7071f9e6af0f73f1f49b99bf293
sydnief <sydniefarrell@gmail.com>
2023-03-03 19:21:05 -0500

    deleted commented code

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,35 +9,6 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   // 
-  
-  // // // select save button for each element
-  // var saveButton = $('#hour-9').children('.saveBtn').eq(2);
-
-  // // select user input
-  // var contentNine = $('.nineAm-text')
-  // // var contentTen = $('.tenAm-text').textValue;
-  // // var contentEleven = $('.elevenAm-text').textValue;
-  // // var contentTwelve = $('.twelvePm-text').textValue;
-  // // var contentOne = $('.onePm-text').textValue;
-  // // var contentTwo = $('.twoPm-text').textValue;
-  // // var contentThree = $('.threePm-text').textValue;
-  // // var contentFour = $('.fourPm-text').textValue;
-  // // var contentFive = $('.fivePm-text').textValue;
-
-  // // // save to local storage
-  // // localStorage.setItem("nineAm", contentNine);
-  // // localStorage.setItem("tenAm", contentTen);
-  // // localStorage.setItem("elevenAm", contentEleven);
-  // // localStorage.setItem("twelvePm", contentTwelve);
-  // // localStorage.setItem("onePm", contentOne);
-  // // localStorage.setItem("twoPm", contentTwo);
-  // // localStorage.setItem("threePm", contentThree);
-  // // localStorage.setItem("fourPm", contentFour);
-  // // localStorage.setItem("fivePm", contentFive);
-
-  // saveButton.on('click', function () {
-  //   localStorage.setItem("nineAm", contentNine.textValue);
-  // });
 
   // Set color of text boxes
   // 9 AM
